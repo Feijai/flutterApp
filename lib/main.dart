9b007964680d2e1003a56d4c5d3b4c3f60d91345
altuve <chickbooman@yahoo.com.tw>
2022-12-12 18:30:02 +0800

    新增頭像

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.pinkAccent[100],
-        appBar: AppBar(
-          title: Text('I AM RICH'),
-          backgroundColor: Colors.blueGrey[900],
-        ),
-        body: Center(
-          child: Image(image: AssetImage('assets/diamond.png')),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            // Row 也可以試試
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('assets/lufy.png'),
+                backgroundColor: Colors.white,
+              ),
+              Text("Luffy D Monkey",
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    fontFamily: 'C',
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white,
+                  ))
+            ],
+          ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
