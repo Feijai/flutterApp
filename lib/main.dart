b4fc54952b01f0bebe77995242930e5589e0b9f3
altuve <chickbooman@yahoo.com.tw>
2022-12-13 14:52:57 +0800

    continue listen

--- lib/main.dart
+++ lib/main.dart
@@ -22,10 +22,60 @@ class MyApp extends StatelessWidget {
               Text("Luffy D Monkey",
                   style: TextStyle(
                     fontSize: 40.0,
-                    fontFamily: 'C',
+                    fontFamily: 'Cubic',
                     fontWeight: FontWeight.w700,
                     color: Colors.white,
-                  ))
+                  )),
+              Text("i am a monkey ",
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontFamily: 'SourceSansPro',
+                    color: Colors.grey,
+                    letterSpacing: 1.5,
+                    fontWeight: FontWeight.bold,
+                  )),
+              Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10,horizontal: 25.0),
+                  child:Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.phone_android,
+                          color: Colors.pink,
+                          size: 50.0,
+                          semanticLabel: 'heart',
+                        ),
+                        SizedBox(width: 10.0,),
+                        Text('09351234567',style: TextStyle(
+                          color: Colors.teal,
+                          fontSize: 25.0,
+                          fontFamily: 'Cubic',
+                          fontWeight: FontWeight.bold,
+                        ),)
+                      ]
+                  )
+              ),
+              Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10,horizontal: 25.0),
+                  child:Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.flood,
+                          color: Colors.teal[900],
+                          size: 50.0,
+                          semanticLabel: 'heart',
+                        ),
+                        SizedBox(width: 10.0,),
+                        Text('火星街京都三年阪二段66號',style: TextStyle(
+                          color: Colors.teal,
+                          fontSize: 20.0,
+                          fontFamily: 'Cubic',
+                          fontWeight: FontWeight.bold,
+                        ),)
+                      ]
+                  )
+              )
             ],
           ),
         ),
