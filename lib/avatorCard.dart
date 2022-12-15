8a84df386155ad05d3011f96595881a25c386491
altuve <chickbooman@yahoo.com.tw>
2022-12-15 14:03:59 +0800

    完成骰子

---
+++ lib/avatorCard.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            // Row 也可以試試
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('assets/lufy.png'),
+                backgroundColor: Colors.white,
+              ),
+              Text("Luffy D Monkey",
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    fontFamily: 'Cubic',
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white,
+                  )),
+              Text("i am a monkey ",
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontFamily: 'SourceSansPro',
+                    color: Colors.grey,
+                    letterSpacing: 1.5,
+                    fontWeight: FontWeight.bold,
+                  )),
+              SizedBox(
+                height: 50.0,
+                child: Divider(
+                    thickness: 1,
+                    color: Colors.teal.shade100
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25.0),
+                child: Padding(
+                  padding: EdgeInsets.all(5.0),
+                  child: ListTile(
+                      leading: Icon(
+                        Icons.phone_android,
+                        color: Colors.pink,
+                        size: 50.0,
+                        semanticLabel: 'heart',
+                      ),
+                      title: Text(
+                        '09351234567',
+                        style: TextStyle(
+                          color: Colors.teal,
+                          fontSize: 25.0,
+                          fontFamily: 'Cubic',
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )),
+                ),
+              ),
+              Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25.0),
+                  child: ListTile(
+                      leading: Icon(
+                        Icons.flood,
+                        color: Colors.teal[900],
+                        size: 50.0,
+                        semanticLabel: 'heart',
+                      ),
+                      title: Text('火星街京都三年阪二段66號',
+                          style: TextStyle(
+                            color: Colors.teal,
+                            fontSize: 20.0,
+                            fontFamily: 'Cubic',
+                            fontWeight: FontWeight.bold,
+                          )))),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,91 +1,89 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  runApp(MyApp());
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          leading: const Icon(Icons.diamond),
+          title: const Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: const DicePage(),
+      ),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDice = 1;
+  int rightDice = 1;
+
+  int random(int min ,int max){
+    return min+ Random().nextInt(max-min);
+  }
+  void getDice(){
+    setState((){
+      leftDice=random(1,7);
+      rightDice=random(1,7);
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Column(
-            // Row 也可以試試
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage('assets/lufy.png'),
-                backgroundColor: Colors.white,
-              ),
-              Text("Luffy D Monkey",
-                  style: TextStyle(
-                    fontSize: 40.0,
-                    fontFamily: 'Cubic',
-                    fontWeight: FontWeight.w700,
-                    color: Colors.white,
-                  )),
-              Text("i am a monkey ",
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontFamily: 'SourceSansPro',
-                    color: Colors.grey,
-                    letterSpacing: 1.5,
-                    fontWeight: FontWeight.bold,
-                  )),
-              SizedBox(
-                height: 50.0,
-                child: Divider(
-                  thickness: 1,
-                    color: Colors.teal.shade100
+
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            // flex: 2,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    padding: const EdgeInsets.all(0),
+
+                  ),
+                  onPressed:getDice,
+                  child: Image.asset('assets/dice$leftDice.png'),
                 ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25.0),
-                child: Padding(
-                  padding: EdgeInsets.all(5.0),
-                  child: ListTile(
-                      leading: Icon(
-                        Icons.phone_android,
-                        color: Colors.pink,
-                        size: 50.0,
-                        semanticLabel: 'heart',
-                      ),
-                      title: Text(
-                        '09351234567',
-                        style: TextStyle(
-                          color: Colors.teal,
-                          fontSize: 25.0,
-                          fontFamily: 'Cubic',
-                          fontWeight: FontWeight.bold,
-                        ),
-                      )),
-                ),
-              ),
-              Card(
-                  color: Colors.white,
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25.0),
-                  child: ListTile(
-                      leading: Icon(
-                        Icons.flood,
-                        color: Colors.teal[900],
-                        size: 50.0,
-                        semanticLabel: 'heart',
-                      ),
-                      title: Text('火星街京都三年阪二段66號',
-                          style: TextStyle(
-                            color: Colors.teal,
-                            fontSize: 20.0,
-                            fontFamily: 'Cubic',
-                            fontWeight: FontWeight.bold,
-                          )))),
-            ],
+              )
           ),
-        ),
+          const SizedBox(width:20.0),
+          Expanded(
+            // flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: MaterialButton(
+                  padding: const EdgeInsets.all(0.0),
+                  onPressed: (){
+                    getDice();
+                  },
+                  child: Image.asset('assets/dice$rightDice.png'),
+                ),
+              ))
+        ],
       ),
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
